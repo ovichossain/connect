e48c04f63b0fa1e2cd6c0b5e5b20a285df723b79
ovichossain <s3448687@student.rmit.edu.au>
2013-08-18 16:33:26 +1000

    Create wineSearch.php

---
+++ wineSearch.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<html>
+<head>
+<title>Wine Search Directory</title>
+</head>
+
+<body>
+<form action="wineSearch.php" method="POST">
+<font size="+2">Wine Name:</font><input type="text" name='WineName'/>
+</form>
+</body>
+</html>
