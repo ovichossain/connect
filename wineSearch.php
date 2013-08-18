a7d541613dafb2bb29f9cfb9c4d8476a7008b8c4
ovichossain <s3448687@student.rmit.edu.au>
2013-08-18 18:46:09 +1000

    Update wineSearch.php

--- wineSearch.php
+++ wineSearch.php
@@ -1,4 +1,23 @@
 <?php
+$searchResult = "";
+if(isset ($_POST['WineName']) && $_POST['WineName'] != ""){
+          $WineName = preg_replace('#[^a-z 0-9]#i','',$_POST['WineName']);
+          $sqlCommand = "SELECT wine_name From wine WHERE wine_name LIKE '%$WineorName'";
+          
+          include_once("connect.php");//requesting to open the database
+          $query = mysql_query($sqlCommand) or die(mysql_error());
+          $count = mysql_num_rows($query);
+          if($count > 1){
+            $searchResult .= "$count results for $WineName $sqlCommand";
+            while($row = mysql_fetch_array($query)){
+              $wName = $row["wine_name"];
+              $searchResult .= "Wine Name: $wName<br />";
+            }//close while
+          }else { $searchResults = "0 results for $WineName $sqlCommand";
+           }
+          
+}
+          
 ?>
 <html>
 <head>
@@ -7,7 +26,11 @@
 
 <body>
 <form action="wineSearch.php" method="POST">
-<font size="+2">Wine Name:</font><input type="text" name='WineName'/>
+<font size="+2">Wine Name:</font><input type="text" name="WineName"/>
+<br />
+<input type="submit" name="searchButton" />
 </form>
+<br />
+<?php echo $serachResult; ?>
 </body>
 </html>
