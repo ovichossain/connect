700cdd8b0dc11f204e92bf067b56a45dc0c12183
ovichossain <s3448687@student.rmit.edu.au>
2013-08-20 01:36:11 +1000

    Update wineSearch.php

--- wineSearch.php
+++ wineSearch.php
@@ -1,17 +1,20 @@
 <?php
 $searchResult = "";
-if(isset ($_POST['WineName']) && $_POST['WineName'] != ""){
+if(isset (($_POST['WineName']) && $_POST['WineName'] != "") or ($_POST['winery']) && $_POST['winery'] != "")){
           $WineName = preg_replace('#[^a-z]#i','',$_POST['WineName']);
-          $sqlCommand = "SELECT wine_name From wine WHERE wine_name LIKE '%$WineName%'";
+          $winery = preg_replace('#[^a-z]#i','',$_POST['winery']);
+          $sqlCommand = "(SELECT wine_name From wine WHERE wine_name LIKE '%$WineName%')UNION
+          (SELECT winery_name From winery WHERE winery_name LIKE '%$WineName%')";
           
           include_once("connect.php");//requesting to open the database
           $query = mysql_query($sqlCommand) or die(mysql_error());
           $count = mysql_num_rows($query);
           if($count > 1){
-            $searchResult .= "$count results for $WineName $sqlCommand";
+            $searchResult .= "$count results for $WineName, $winery $sqlCommand";
             while($row = mysql_fetch_array($query)){
               $wName = $row["wine_name"];
-              $searchResult .= "Wine Name: $wName<br />";
+              $Winery = $row["winery_name"];
+              $searchResult .= "Wine Name: $wName Winery Name: $Winery<br />";
             }//close while
           }else { $searchResults = "0 results for $WineName $sqlCommand";
            }
@@ -27,6 +30,8 @@ if(isset ($_POST['WineName']) && $_POST['WineName'] != ""){
 <body>
 <form action="wineSearch.php" method="POST">
 <font size="+2">Wine Name:</font><input type="text" name="WineName"/>
+<br />
+<font size="+2">Winery   :</font><input type="text" name="winery" />
 <br />
 <input type="submit" name="searchButton" />
 </form>
