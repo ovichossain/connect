caa59038a9b487003aa22dad1570fb3da42615d8
ovichossain <s3448687@student.rmit.edu.au>
2013-08-21 02:54:22 +1000

    Update wineSearch.php

--- wineSearch.php
+++ wineSearch.php
@@ -2,26 +2,41 @@
 $searchResult = "";
 //or ($_POST['winery']) && $_POST['winery'] != "")
 
-if(isset ($_POST['WineName']) && $_POST['WineName'] != ""){
-          $WineName = preg_replace('#[^a-z]#i','',$_POST['WineName']);
+//if(isset ($_POST['WineName']) && $_POST['WineName'] != ""){
+          //$WineName = preg_replace('#[^a-z]#i','',$_POST['WineName']);
           //$winery = preg_replace('#[^a-z]#i','',$_POST['winery']);
-          $sqlCommand = "(SELECT wine_name From wine WHERE wine_name LIKE '%$WineName%')";
+          //$sqlCommand = "(SELECT wine_name From wine WHERE wine_name LIKE '%$WineName%')";
           //UNION(SELECT winery_name From winery WHERE winery_name LIKE '%$winery%')";
+          $arr = new array();
+          if(!empty($_POST['WineName']))
+          $arr[] = "wine_name LIKE '".$_POST['WineName']."'";
+          if(!empty($_POST['winery']))
+          $arr[] = "winery_name LIKE '".$_POST['winery']."'";
+          
+          $str = impode("and", $arr);
+          if(!empty($str)) $str = "and ".$str;
           
           include_once("connect.php");//requesting to open the database
-          $query = mysql_query($sqlCommand) or die(mysql_error());
+          $query = mysql_query("select * from WineSearchView where 1 $str") or die(mysql_error());
           $count = mysql_num_rows($query);
           if($count > 1){
-            $searchResult .= "$count results for $WineName, $sqlCommand";
+            $searchResult .= "$count results for $WineName";
             while($row = mysql_fetch_array($query)){
               $wName = $row["wine_name"];
+              $variety = $row["variety"];
+              $year = $row["year"];
+              $wineryName = $row["winery_name"];
+              $region = $row["region_name"];
+              $cost = $row["cost"];
+              $stock = $row["on_hand"];
+              $ordered = $row["qty"];
               //$Winery = $row["winery_name"];
-              $searchResult .= "Wine Name: $wName <br />";
+              $searchResult .= "Wine Name: $wName, V: $variety, Y: $year, WN: $winery_name, R: $region, C: $cost, S: $stock, O: $ordered<br />";
             }//close while
           }else { $searchResults = "0 results for $WineName $sqlCommand";
            }
           
-}
+//}
           
 ?>
 <html>
